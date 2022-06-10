2a9aed4c2420305400384f512f4185a6a17faa6d
MrBarbo <69595554+MrBarbo@users.noreply.github.com>
2022-06-10 17:56:09 -0300

    Statistics
    Estadisticas e inicio del predictor

--- public/javascripts/prediction.js
+++ public/javascripts/prediction.js
@@ -7,10 +7,38 @@ const CoinGeckoClient = new CoinGecko();
 //3. Make calls
 var predict = async() => {
   let data = await CoinGeckoClient.ping();
-  let data2 = await CoinGeckoClient.coins.fetchMarketChart('bitcoin');
-  let hola = data2.data;
-  console.log(hola)
+  const datos =[]
+  const Objeto = {
+    days: '120',
+    vs_currency: 'usd'
+  }
+  let data2 = await CoinGeckoClient.coins.fetchMarketChart('bitcoin', Objeto);
+  console.log("Textazo");
+  for (var i = 0; i < 10; i++) {
+    datos[i]= data2.data.prices[i][1];
+ }
+ let sts = statistics(datos);
+ /*for (var i = 10; i < 20; i++) {
+  if(datos[i-2]>datos[i-1]){
+    datos[i]= 1 -()+random
+  }else
+}*/
+  
+  console.log(datos);
+  
+
 };
+
+function statistics(data){
+  let max = -1; let min = Number.MAX_VALUE; let total = 0;
+  for (var i = 0; i < data.length; i++) {
+    if (data[i]>max) {max = data[i]};
+    if (data[i]<min) {min = data[i]};
+    total += data[i];
+ }
+ const ret=[min,max,total/data.length]
+ return ret
+}
 
 predict();
 
