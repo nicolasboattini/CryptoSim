b5781d76355a92e86b11ded232e81034e7cc7744
MrBarbo <ivan_barboza@outlook.com>
2022-06-15 21:29:30 -0300

    Funciones lógicas

--- public/javascripts/frontend.js
+++ public/javascripts/frontend.js
@@ -20,40 +20,50 @@ function func(e) {
     e.target.click()})
 
 
-//Gráfico
+    //Gráfico
 
-var xyValues = [
-  { x: 50, y: 7 },
-  { x: 60, y: 8 },
-  { x: 70, y: 8 },
-  { x: 80, y: 9 },
-  { x: 90, y: 9 },
-  { x: 100, y: 9 },
-  { x: 110, y: 10 },
-  { x: 120, y: 11 },
-  { x: 130, y: 14 },
-  { x: 140, y: 14 },
-  { x: 150, y: 15 },
-]
-const ctx = document.getElementById("myChart").getContext("2d")
 
-const myChart = new Chart(ctx, {
-  type: "scatter",
-  data: {
-    datasets: [
-      {
-        pointRadius: 4,
-        pointBackgroundColor: "rgb(0,0,255)",
-        data: xyValues,
+    var xyValues = [89,24,65,45,NaN,NaN];
+    var xyValues2 = [NaN,NaN,NaN,45,24,46];
+    var labels= []
+    for (i=0;i<6;i++){
+      labels[i]=i-3+' dias';
+    }
+    const ctx = document.getElementById("myChart").getContext("2d")
+    
+    const myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        datasets: [
+          {
+            pointRadius: 4,
+            pointBackgroundColor: "rgb(0,0,255)",
+            data: xyValues,
+          },
+        ],
+        labels: labels,
+        datasets: [{
+          label: 'Precio Pasado',
+          data: xyValues,
+          fill: true,
+          borderColor: 'rgb(255, 35, 1)',
+          tension: 0.1
+        },{
+          label: 'Precio Futuro',
+          data: xyValues2,
+          fill: true,
+          borderColor: 'rgb(1, 35, 1)',
+          tension: 0.1
+        }],
       },
-    ],
-  },
-  options: {
-    legend: { display: false },
-    scales: {
-      xAxes: [{ ticks: { min: 40, max: 160 } }],
-      yAxes: [{ ticks: { min: 6, max: 16 } }],
-    },
-  },
-})
-
+      options: {
+        responsive:true,
+        legend: { display: false },
+        scales: {
+          xAxes: [{ ticks: { min: 40, max: 160 } }],
+          yAxes: [{ ticks: { min: 6, max: 16 } }],
+          xAxes: [{ ticks: { min: -10, max: 120 } }],
+          yAxes: [{ ticks: { min: 0, max: 90 } }],
+        },
+      },
+    });
