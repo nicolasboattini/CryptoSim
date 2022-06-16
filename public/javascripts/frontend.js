1eab0b4d078640647c6fe75bf504341ffb45a5fe
nicolasboattini <38139882+nicolasboattini@users.noreply.github.com>
2022-06-16 00:35:04 -0300

    show data when pressing simular button

--- public/javascripts/frontend.js
+++ public/javascripts/frontend.js
@@ -1,15 +1,22 @@
+//global variable
+var graficaData;
+
+//Change grafica dropdown display to selected item
 $(document).ready(function(){
   $('#graficas_drop a').click(function(){
     $('#selectedGrafica').text($(this).text());
+    graficaData = $(this).data();
     });
 });
 
+//Change criptomoneda dropdown display to selected item
 $(document).ready(function(){
   $('#cripto_drop a').click(function(){
     $('#selectedCripto').text($(this).text());
     });
 });
 
+//Set costoKw to placeholder when value is 0
 function func(e) {
   if(e.target.value==e.target.getAttribute("min")){
     e.target.value="";
@@ -18,68 +25,8 @@ function func(e) {
 document.getElementById("costoKW").addEventListener('click', func);
 document.getElementById("costoKW").addEventListener('keyup', function(e){e.target.click()});
 
-    //Gráfico
 
-var xyValues = [
-  { x: 50, y: 7 },
-  { x: 60, y: 8 },
-  { x: 70, y: 8 },
-  { x: 80, y: 9 },
-  { x: 90, y: 9 },
-  { x: 100, y: 9 },
-  { x: 110, y: 10 },
-  { x: 120, y: 11 },
-  { x: 130, y: 14 },
-  { x: 140, y: 14 },
-  { x: 150, y: 15 },
-];
-
-const ctx = document.getElementById("myChart").getContext("2d");
-const myChart = new Chart(ctx, {
-  type: "scatter",
-  data: {
-    datasets: [
-      {
-        pointRadius: 4,
-        pointBackgroundColor: "rgb(0,0,255)",
-        data: xyValues,
-      },
-    ],
-  },
-  options: {
-    legend: { display: false },
-    scales: {
-      xAxes: [{ ticks: { min: 40, max: 160 } }],
-      yAxes: [{ ticks: { min: 6, max: 16 } }],
-    },
-  },
-});
-//Grafico
-
-$(document).ready(function (){
-  $('#simular').click(function(){
-    /*var graficaId = $('#graficas_drop option:selected').data('id');
-    var graficaHasheo = $('#graficas_drop option:selected').data('frecuenciaDeHasheo');
-    var graficaPrecio = $('#graficas_drop option:selected').data('precio');
-    var graficaConsumo = $('#graficas_drop option:selected').data('consumo');*/
-    var graficaData= $('#graficas_drop option:selected').data();
-	
-    console.log(graficaData);
-    
-    console.log(graficaData.id);
-    console.log(graficaData.frecuenciaDeHasheo);
-    console.log(graficaData.precio); 
-    console.log(graficaData.consumo);
-    var cantidad = $('#grafica_cantidad').val();
-    //console.log('%d, %d, %d, %d, %d', graficaId, graficaHasheo, graficaPrecio, graficaConsumo, cantidad);
-    var precio = graficaPrecio * cantidad;
-    var consumo = graficaConsumo * cantidad;
-    var hasheo = graficaHasheo * cantidad;
-    $('#consumo_res').text(consumo);    
-    $('#hasheo_res').text(hasheo);
-  });
-});
-
+/*Gráfico start*/
     var xyValues = [89,24,65,45,NaN,NaN];
     var xyValues2 = [NaN,NaN,NaN,45,24,46];
     var labels= []
@@ -124,3 +71,57 @@ $(document).ready(function (){
         },
       },
     });
+/*Grafico End*/
+
+/*$(document).ready(function (){
+  $('#simular').click(function(){
+    /*var graficaId = $('#graficas_drop option:selected').data('id');
+    var graficaHasheo = $('#graficas_drop option:selected').data('frecuenciaDeHasheo');
+    var graficaPrecio = $('#graficas_drop option:selected').data('precio');
+    var graficaConsumo = $('#graficas_drop option:selected').data('consumo');
+    var graficaData= $('#graficas_drop option:selected').data();
+	
+    console.log(graficaData);
+    
+    console.log(graficaData.id);
+    console.log(graficaData.frecuenciaDeHasheo);
+    console.log(graficaData.precio); 
+    console.log(graficaData.consumo);
+    var cantidad = $('#grafica_cantidad').val();
+    //console.log('%d, %d, %d, %d, %d', graficaId, graficaHasheo, graficaPrecio, graficaConsumo, cantidad);
+    var precio = graficaPrecio * cantidad;
+    var consumo = graficaConsumo * cantidad;
+    var hasheo = graficaHasheo * cantidad;
+    $('#consumo_res').text(consumo);    
+    $('#hasheo_res').text(hasheo);
+  });
+});*/
+
+$(document).ready(function (){
+  $('#simular').click(function(){
+   
+	  var graficaNombre = $('#selectedGrafica').text();  
+	  
+   console.log(graficaData);
+   
+   //console.log(graficaData.id);
+   //console.log(graficaData.frecuenciadehasheo);
+   //console.log(graficaData.precio);
+   //console.log(graficaData.consumo);
+   
+   var cantidad = parseInt($('#grafica_cantidad').val());
+   
+   console.log(cantidad);
+   
+    var precio = graficaData.precio * cantidad;
+    var consumo = graficaData.consumo * cantidad;
+    var hasheo = graficaData.frecuenciadehasheo * cantidad;
+    
+    $('#consumo_res').text(consumo);    
+    $('#hasheo_res').text(hasheo);
+  
+	
+  });
+});
+
+    
